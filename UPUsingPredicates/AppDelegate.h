c15629a114b6b51cabad34f8d433762868621b10
andrey.karaban <andrey.karaban@mobexs.com>
2016-02-29 09:59:22 +0300

    initial commit

---
+++ UPUsingPredicates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UPUsingPredicates
+//
+//  Created by Mobexs on 2/29/16.
+//  Copyright © 2016 AnkariOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UPUsingPredicates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UPUsingPredicates
+//
+//  Created by Mobexs on 2/29/16.
+//  Copyright © 2016 AnkariOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
